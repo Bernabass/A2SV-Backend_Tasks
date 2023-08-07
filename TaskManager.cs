fe2ffb69200518ce289c78700d8515b572591650
Bernabass <99589215+Bernabass@users.noreply.github.com>
2023-08-07 10:16:54 +0300

    Day 4 Task 1

--- TaskManager.cs
+++ TaskManager.cs
@@ -1,106 +1,138 @@
-public enum TaskCategory{
+enum TaskCategory{
     Personal,
     Work,
     Errands
 }
 
-public class TodoTask{
+class TaskItem{
     public string Name { get; set; }
     public string Description { get; set; }
     public TaskCategory Category { get; set; }
     public bool IsCompleted { get; set; }
+}
 
-    public TodoTask(string name, string description, TaskCategory category){
-        Name = name;
-        Description = description;
-        Category = category;
-        IsCompleted = false;
+class TaskManager{
+    private List<TaskItem> tasks = new List<TaskItem>();
+
+    public void AddTask(TaskItem task){
+        tasks.Add(task);
+    }
+
+    public void ViewTasks(Func<TaskItem, bool> filter = null){
+        var filteredTasks = filter != null ? tasks.Where(filter) : tasks;
+
+        if (!filteredTasks.Any()){
+            Console.WriteLine("No tasks found.");
+            return;
+        }
+
+        foreach (var task in filteredTasks){
+            Console.WriteLine($"Name: {task.Name}");
+            Console.WriteLine($"Description: {task.Description}");
+            Console.WriteLine($"Category: {task.Category}");
+            Console.WriteLine($"Is Completed: {task.IsCompleted}");
+            Console.WriteLine();
+        }
+    }
+
+    public async Task SaveTasksToFileAsync(string filePath){
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            foreach (var task in tasks)
+            {
+                await writer.WriteLineAsync($"{task.Name},{task.Description},{task.Category},{task.IsCompleted}");
+            }
+        }
+    }
+
+    public async Task LoadTasksFromFileAsync(string filePath){
+        if (!File.Exists(filePath))
+            return;
+
+        using (StreamReader reader = new StreamReader(filePath)){
+            string line;
+            while ((line = await reader.ReadLineAsync()) != null){
+                string[] parts = line.Split(',');
+                
+                if (parts.Length == 4){
+                    TaskItem task = new TaskItem{
+                        Name = parts[0],
+                        Description = parts[1],
+                        Category = (TaskCategory)Enum.Parse(typeof(TaskCategory), parts[2], true),
+                        IsCompleted = bool.Parse(parts[3])
+                    };
+
+                    tasks.Add(task);
+                }
+            }
+        }
     }
 }
 
-class DemoTasks{
-    const string FilePath = "tasks.csv";
-
+class Program
+{
     static async Task Main(string[] args){
-        
-        List<TodoTask> tasks = await ReadTasksFromFileAsync();
+        TaskManager taskManager = new TaskManager();
 
-        Console.WriteLine("Tasks:");
-        foreach (TodoTask task in tasks){
-            Console.WriteLine($"- {task.Name} ({task.Category}, {task.Description})");
-        }
+        // Load tasks from file asynchronously
+        await taskManager.LoadTasksFromFileAsync("tasks.csv");
 
-        Console.WriteLine("\nEnter a category to filter tasks (Personal, Work, or Errands): ");
-        string category = Console.ReadLine();
+        while (true){
+            Console.WriteLine("\nTask Manager");
+            Console.WriteLine("------------------");
+            Console.WriteLine("1. Add Task");
+            Console.WriteLine("2. View All Tasks");
+            Console.WriteLine("3. View Tasks by Category");
+            Console.WriteLine("4. Save and Exit");
 
-        if (Enum.TryParse(category, true, out TaskCategory taskCategory)){
-            List<TodoTask> filteredTasks = tasks.Where(task => task.Category == taskCategory).ToList();
-            Console.WriteLine("Filtered tasks:");
 
-            foreach (TodoTask task in filteredTasks){
-                Console.WriteLine($"- {task.Name} ({task.Category}, {task.Description})");
-            }
-        }
-        else
-            Console.WriteLine("Invalid category.");
+            Console.Write("\nSelect an option: ");
+            string choice = Console.ReadLine();
 
-        Console.WriteLine("\nEnter a new task name: ");
-        string newName = Console.ReadLine();
-
-        Console.WriteLine("Enter a new task description: ");
-        string newDescription = Console.ReadLine();
-
-        Console.WriteLine("Enter the category for the new task (Personal, Work, or Errands): ");
-        string newCategory = Console.ReadLine();
-
-        if (Enum.TryParse(newCategory, true, out TaskCategory newTaskCategory)){
-            TodoTask newTask = new TodoTask(newName, newDescription, newTaskCategory);
-            tasks.Add(newTask);
-
-            await SaveTasksToFileAsync(tasks);
-
-            Console.WriteLine("New task added successfully!");
-        }
-        else
-            Console.WriteLine("Invalid category. Task not added.");
-
-    }
-
-    static async Task SaveTasksToFileAsync(List<TodoTask> tasks){
-        try{
-            using (StreamWriter writer = new StreamWriter(FilePath))
-            {
-                foreach (TodoTask task in tasks){
-                    await writer.WriteLineAsync($"{task.Name},{task.Description},{task.Category},{task.IsCompleted}");
-                }
-            }
-        }
-        catch (Exception ex){
-            Console.WriteLine($"Error writing to file: {ex.Message}");
-        }
-    }
-    static async Task<List<TodoTask>> ReadTasksFromFileAsync(){
-        List<TodoTask> tasks = new List<TodoTask>();
-
-        try{
-            using (StreamReader reader = new StreamReader(FilePath))
-            {
-                string line;
-                while ((line = await reader.ReadLineAsync()) != null)
-                {
-                    string[] parts = line.Split(',');
-                    if (parts.Length == 4 && Enum.TryParse(parts[2], true, out TaskCategory category)){
-                        
-                        tasks.Add(new TodoTask(parts[0], parts[1], category));
+            switch (choice){
+                case "1":
+                    // Add Task
+                    Console.Write("Task Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Description: ");
+                    string description = Console.ReadLine();
+                    Console.Write("Category (Personal/Work/Errands): ");
+                    if (Enum.TryParse(Console.ReadLine(), true, out TaskCategory category)){
+                    
+                        TaskItem newTask = new TaskItem { Name = name, Description = description, Category = category };
+                        taskManager.AddTask(newTask);
+                        Console.WriteLine("Task added successfully.");
                     }
-                }
+                    else
+                        Console.WriteLine("Invalid category.");
+
+                    break;
+
+                case "2":
+                    // View All Tasks
+                    taskManager.ViewTasks();
+                    break;
+
+                case "3":
+                    // View Tasks by Category
+                    Console.Write("Category (Personal/Work/Errands): ");
+                    if (Enum.TryParse(Console.ReadLine(), true, out TaskCategory filterCategory)){
+                        taskManager.ViewTasks(task => task.Category == filterCategory);
+                    }
+                    else
+                        Console.WriteLine("Invalid category.");
+                    break;
+
+                case "4":
+                    // Save and Exit
+                    await taskManager.SaveTasksToFileAsync("tasks.csv");
+                    Console.WriteLine("Tasks saved to file. Good bye...");
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please select a valid option.");
+                    break;
             }
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error reading from file: {ex.Message}");
-        }
-
-        return tasks;
     }
 }
